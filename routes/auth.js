1ddf5e2487dfae20c7b76a1c11e5980e314065e1
57hemanth <hemanthchowdary1234@gmail.com>
2025-03-03 23:38:26 +0530

    Added email, password authentication

--- routes/auth.js
+++ routes/auth.js
@@ -5,11 +5,15 @@ const {
   refreshToken,
   checkUsernameAvailability,
   signUpWithOauth,
+  signUpWithEmail,
+  loginWithEmail
 } = require("../controllers/auth/auth");
 
 router.post("/check-username", checkUsernameAvailability);
 router.post("/login", signInWithOauth);
 router.post("/register", signUpWithOauth);
 router.post("/refresh-token", refreshToken);
+router.post("/register-email", signUpWithEmail );
+router.post("/login-email", loginWithEmail );
 
 module.exports = router;
